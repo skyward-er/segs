ea6a7075954b50595d2b38834beddecabf281f07
Federico Lolli <federico.lolli@skywarder.eu>
2024-11-14 18:50:48 +0100

    [ui] Added utils.rs with vertical_centering helper function with double pass

--- src/ui/utils.rs
+++ src/ui/utils.rs
@@ -1,5 +1,5 @@
 use egui::containers::Frame;
-use egui::{Shadow, Stroke, Style, Ui};
+use egui::{Response, Shadow, Stroke, Style, Ui};
 
 use super::panes::{Pane, PaneBehavior};
 
@@ -11,4 +11,33 @@ pub fn maximized_pane_ui(ui: &mut Ui, pane: &mut Pane) {
         .shadow(Shadow::NONE)
         .stroke(Stroke::NONE)
         .show(ui, |ui| pane.ui(ui));
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct SizingMemo {
+    occupied_height: f32,
+    sizing_pass_done: bool,
+}
+
+pub fn vertically_centered(
+    ui: &mut Ui,
+    memo: &mut SizingMemo,
+    add_contents: impl FnOnce(&mut Ui) -> Response,
+) -> egui::Response {
+    if !memo.sizing_pass_done {
+        let r = add_contents(ui);
+        memo.occupied_height = r.rect.height();
+        memo.sizing_pass_done = true;
+        ui.ctx()
+            .request_discard("horizontally_centered requires a sizing pass");
+        r
+    } else {
+        let spacing = (ui.available_height() - memo.occupied_height) / 2.0;
+        ui.vertical_centered(|ui| {
+            ui.add_space(spacing);
+            add_contents(ui);
+            ui.add_space(spacing);
+        })
+        .response
+    }
 }
