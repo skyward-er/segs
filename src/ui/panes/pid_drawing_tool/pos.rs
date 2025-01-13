ced9b097ff0217db6013ee4b57399bd994f1d89c
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2025-01-13 18:56:45 +0100

    Improved PID centering

--- src/ui/panes/pid_drawing_tool/pos.rs
+++ src/ui/panes/pid_drawing_tool/pos.rs
@@ -10,6 +10,10 @@ pub struct Pos {
 }
 
 impl Pos {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
     pub fn add_size(&self, size: i32) -> Self {
         Self {
             x: self.x + size,
