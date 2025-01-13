01deb2d1b3672413a6d8af2a5a777216fb7b38f6
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2025-01-13 12:06:30 +0100

    Implemented different ancor points for each PID elements

--- src/ui/panes/pid_drawing_tool/symbols.rs
+++ src/ui/panes/pid_drawing_tool/symbols.rs
@@ -4,18 +4,27 @@ use strum_macros::{Display, EnumIter};
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, EnumIter, Display)]
 pub enum Symbol {
-    BallValve,
+    ManualValve,
     CheckValve,
+    // ReliefValve,
+    // ControlValve,
+    // PressureRegulator,
+    // BurstDisk,
+    // QuickConnector,
+    // PressureTransducer,
+    // PressureGauge,
+    // FlexibleConnection,
+    // ThreeWayValve,
     PressurizedVessel,
 }
 
 impl Symbol {
     pub fn get_image(&self, theme: Theme) -> ImageSource {
         match (&self, theme) {
-            (Symbol::BallValve, Theme::Light) => {
+            (Symbol::ManualValve, Theme::Light) => {
                 egui::include_image!("../../../../icons/ball_valve_light.svg")
             }
-            (Symbol::BallValve, Theme::Dark) => {
+            (Symbol::ManualValve, Theme::Dark) => {
                 egui::include_image!("../../../../icons/ball_valve_dark.svg")
             }
             (Symbol::CheckValve, Theme::Light) => {
@@ -30,6 +39,14 @@ impl Symbol {
             (Symbol::PressurizedVessel, Theme::Dark) => {
                 egui::include_image!("../../../../icons/pressurized_vessel_dark.svg")
             }
+        }
+    }
+
+    pub fn get_ancor_points(&self) -> Vec<(f32, f32)> {
+        match self {
+            Symbol::ManualValve => [(-0.5, 0.0), (0.5, 0.0)].into(),
+            Symbol::CheckValve => [(-0.5, 0.0), (0.5, 0.0)].into(),
+            Symbol::PressurizedVessel => [(0.0, -0.5), (0.0, 0.5)].into(),
         }
     }
 }
