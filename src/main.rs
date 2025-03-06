c17ae80378e8290d9bef67ea271721446537bd58
Federico Lolli <federico.lolli@skywarder.eu>
2025-03-06 10:52:31 +0100

    added logging to file and removed uuid

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,15 @@ static APP_NAME: &str = "segs";
 fn main() -> Result<(), eframe::Error> {
     // Set up logging (USE RUST_LOG=debug to see logs)
     let env_filter = EnvFilter::builder().from_env_lossy();
+    let file_appender = tracing_appender::rolling::daily("logs/", "segs.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer().with_filter(env_filter))
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_writer(non_blocking),
+        )
         .with(tracing_tracy::TracyLayer::default())
         .init();
 
