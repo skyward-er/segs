2192ff952aaa12ca56eeffee624c0061fc099d5c
Federico Lolli <federico.lolli@skywarder.eu>
2025-05-15 21:49:59 +0200

    improved a bit the UI

--- src/ui/panes/command.rs
+++ src/ui/panes/command.rs
@@ -1,15 +1,13 @@
-use std::collections::HashMap;
-
-use egui::{Button, RichText, Sense, Ui, Vec2};
-use egui_extras::{Size, Strip, StripBuilder};
+use egui::{Button, RichText, Sense, Ui};
+use jiff::{Unit, Zoned};
 use mavlink_bindgen::parser::MavType;
-use serde::{Deserialize, Serialize, de::IntoDeserializer};
-use tracing::warn;
+use serde::{Deserialize, Serialize};
+use tracing::{info, warn};
 
 use crate::{
     error::ErrInstrument,
     mavlink::{
-        COMMAND_TC_DATA, MavMessage, Message, MessageData, TimedMessage,
+        MavMessage, Message, TimedMessage,
         reflection::{FieldLike, FieldLookup, MAVLINK_PROFILE, MapConvertible, MessageMap},
     },
     ui::{app::PaneResponse, shortcuts::ShortcutHandler},
@@ -26,6 +24,9 @@ pub struct CommandPane {
 
     #[serde(skip)]
     settings_visible: bool,
+    // TODO handle message responses
+    #[serde(skip)]
+    commands_to_send: Vec<MavMessage>,
 }
 
 impl Default for CommandPane {
@@ -36,6 +37,7 @@ impl Default for CommandPane {
             text_size: 16.0,
             show_only_tc: true,
             settings_visible: false,
+            commands_to_send: Vec::new(),
         }
     }
 }
@@ -66,6 +68,30 @@ impl PaneBehavior for CommandPane {
 
                 // open the menu on right click on button
                 btn_res.context_menu(|ui| command_menu(ui, self));
+                if btn_res.clicked() {
+                    info!("Command {} clicked", self.text);
+                    // send the message
+                    if let Some(message) = self.message.as_ref() {
+                        info!(
+                            "Sending {} message",
+                            MAVLINK_PROFILE
+                                .get_msg(message.message_id())
+                                .log_unwrap()
+                                .name
+                        );
+                        let mut map = message.clone();
+                        if let Some(tm) = map.get_mut_field("timestamp") {
+                            // set the timestamp to the current time
+                            *tm = Zoned::now()
+                                .round(Unit::Nanosecond)
+                                .log_unwrap()
+                                .timestamp()
+                                .as_nanosecond() as u64;
+                        }
+                        self.commands_to_send
+                            .push(MavMessage::from_map(map).log_unwrap());
+                    }
+                }
             })
             .response;
 
@@ -90,6 +116,10 @@ impl PaneBehavior for CommandPane {
 
     fn get_message_subscriptions(&self) -> Box<dyn Iterator<Item = u32>> {
         Box::new(None.into_iter())
+    }
+
+    fn drain_outgoing_messages(&mut self) -> Vec<MavMessage> {
+        self.commands_to_send.drain(..).collect()
     }
 }
 
@@ -154,96 +184,109 @@ fn command_settings(ui: &mut Ui, pane: &mut CommandPane) {
     // For each field in the message, show a text box with the field name and value,
     // and update the MessageMap based on the content of these text fields.
     if let Some(message_map) = pane.message.as_mut() {
-        ui.group(|ui| {
-            for i in 0..message_map.field_map().len() {
-                let field = i
-                    .to_mav_field(message_map.message_id(), &MAVLINK_PROFILE)
-                    .log_unwrap();
+        let mut settable_fields = (0..message_map.field_map().len())
+            .map(|f| {
+                f.to_mav_field(message_map.message_id(), &MAVLINK_PROFILE)
+                    .log_unwrap()
+            })
+            .collect::<Vec<_>>();
 
-                ui.horizontal(|ui| {
-                    ui.label(format!("{}:", &field.field().name.to_uppercase()));
+        // filter out the fields that are not settable
+        settable_fields.retain(|f| {
+            // skip the timestamp field
+            f.field().name.to_lowercase() != "timestamp"
+        });
 
-                    if let Some(enum_type) = &field.field().enumtype {
-                        let enum_info = MAVLINK_PROFILE.get_enum(enum_type).log_unwrap();
-                        // TODO handle enum advanced options
-                        macro_rules! variant_selector_for {
-                            ($kind:ty) => {{
-                                let variant_ix: &mut $kind =
-                                    message_map.get_mut_field(field).log_unwrap();
-                                let selected_text =
-                                    enum_info.entries[*variant_ix as usize].name.clone();
-                                egui::ComboBox::from_id_salt(ui.id().with("field_selector"))
-                                    .selected_text(selected_text)
-                                    .show_ui(ui, |ui| {
-                                        for (index, variant) in enum_info.entries.iter().enumerate()
-                                        {
-                                            ui.selectable_value(
-                                                variant_ix,
-                                                index as $kind,
-                                                &variant.name,
-                                            );
-                                        }
-                                    });
-                            }};
-                        }
+        if !settable_fields.is_empty() {
+            ui.group(|ui| {
+                for field in settable_fields {
+                    ui.horizontal(|ui| {
+                        ui.label(format!("{}:", &field.field().name.to_uppercase()));
 
-                        match field.field().mavtype {
-                            MavType::UInt8 => variant_selector_for!(u8),
-                            MavType::UInt16 => variant_selector_for!(u16),
-                            MavType::UInt32 => variant_selector_for!(u32),
-                            MavType::UInt64 => variant_selector_for!(u64),
-                            _ => {
-                                // TODO handle other enum types
-                                warn!(
-                                    "Enum type {} is not supported for field {}",
-                                    enum_type,
-                                    field.field().name
-                                );
+                        // show the combo box for enum types
+                        if let Some(enum_type) = &field.field().enumtype {
+                            let enum_info = MAVLINK_PROFILE.get_enum(enum_type).log_unwrap();
+                            // TODO handle enum advanced options
+                            macro_rules! variant_selector_for {
+                                ($kind:ty) => {{
+                                    let variant_ix: &mut $kind =
+                                        message_map.get_mut_field(field).log_unwrap();
+                                    let selected_text =
+                                        enum_info.entries[*variant_ix as usize].name.clone();
+                                    egui::ComboBox::from_id_salt(ui.id().with("field_selector"))
+                                        .selected_text(selected_text)
+                                        .show_ui(ui, |ui| {
+                                            for (index, variant) in
+                                                enum_info.entries.iter().enumerate()
+                                            {
+                                                ui.selectable_value(
+                                                    variant_ix,
+                                                    index as $kind,
+                                                    &variant.name,
+                                                );
+                                            }
+                                        });
+                                }};
                             }
-                        }
-                    } else {
-                        macro_rules! drag_value_with_range {
-                            ($_type:ty, $min:expr, $max:expr) => {{
-                                let value: &mut $_type =
-                                    message_map.get_mut_field(field).log_unwrap();
-                                ui.add(egui::DragValue::new(value).range($min..=$max));
-                            }};
-                        }
-
-                        match field.field().mavtype {
-                            MavType::UInt8MavlinkVersion | MavType::UInt8 => {
-                                drag_value_with_range!(u8, 0, u8::MAX)
-                            }
-                            MavType::UInt16 => drag_value_with_range!(u16, 0, u16::MAX),
-                            MavType::UInt32 => drag_value_with_range!(u32, 0, u32::MAX),
-                            MavType::UInt64 => drag_value_with_range!(u64, 0, u64::MAX),
-                            MavType::Int8 => drag_value_with_range!(i8, i8::MIN, i8::MAX),
-                            MavType::Int16 => drag_value_with_range!(i16, i16::MIN, i16::MAX),
-                            MavType::Int32 => drag_value_with_range!(i32, i32::MIN, i32::MAX),
-                            MavType::Int64 => drag_value_with_range!(i64, i64::MIN, i64::MAX),
-                            MavType::Float => drag_value_with_range!(f32, f32::MIN, f32::MAX),
-                            MavType::Double => drag_value_with_range!(f64, f64::MIN, f64::MAX),
-                            MavType::Char => {
-                                let value: &mut char =
-                                    message_map.get_mut_field(field).log_unwrap();
-                                let mut buffer = value.to_string();
-                                ui.add(
-                                    egui::TextEdit::singleline(&mut buffer)
-                                        .hint_text("char")
-                                        .char_limit(1),
-                                );
-                                if let Some(c) = buffer.chars().next() {
-                                    *value = c;
-                                } else {
-                                    warn!("Invalid char input: {}", buffer);
-                                    // TODO handle invalid char input (USER ERROR)
+                            match field.field().mavtype {
+                                MavType::UInt8 => variant_selector_for!(u8),
+                                MavType::UInt16 => variant_selector_for!(u16),
+                                MavType::UInt32 => variant_selector_for!(u32),
+                                MavType::UInt64 => variant_selector_for!(u64),
+                                _ => {
+                                    // TODO handle other enum types
+                                    warn!(
+                                        "Enum type {} is not supported for field {}",
+                                        enum_type,
+                                        field.field().name
+                                    );
                                 }
                             }
-                            MavType::Array(_, _) => warn!("Array types are not supported yet"), // TODO handle array types
+                        } else {
+                            // show the drag value for numeric types and text box for char types
+                            macro_rules! drag_value_with_range {
+                                ($_type:ty, $min:expr, $max:expr) => {{
+                                    let value: &mut $_type =
+                                        message_map.get_mut_field(field).log_unwrap();
+                                    ui.add(egui::DragValue::new(value).range($min..=$max));
+                                }};
+                            }
+
+                            match field.field().mavtype {
+                                MavType::UInt8MavlinkVersion | MavType::UInt8 => {
+                                    drag_value_with_range!(u8, 0, u8::MAX)
+                                }
+                                MavType::UInt16 => drag_value_with_range!(u16, 0, u16::MAX),
+                                MavType::UInt32 => drag_value_with_range!(u32, 0, u32::MAX),
+                                MavType::UInt64 => drag_value_with_range!(u64, 0, u64::MAX),
+                                MavType::Int8 => drag_value_with_range!(i8, i8::MIN, i8::MAX),
+                                MavType::Int16 => drag_value_with_range!(i16, i16::MIN, i16::MAX),
+                                MavType::Int32 => drag_value_with_range!(i32, i32::MIN, i32::MAX),
+                                MavType::Int64 => drag_value_with_range!(i64, i64::MIN, i64::MAX),
+                                MavType::Float => drag_value_with_range!(f32, f32::MIN, f32::MAX),
+                                MavType::Double => drag_value_with_range!(f64, f64::MIN, f64::MAX),
+                                MavType::Char => {
+                                    let value: &mut char =
+                                        message_map.get_mut_field(field).log_unwrap();
+                                    let mut buffer = value.to_string();
+                                    ui.add(
+                                        egui::TextEdit::singleline(&mut buffer)
+                                            .hint_text("char")
+                                            .char_limit(1),
+                                    );
+                                    if let Some(c) = buffer.chars().next() {
+                                        *value = c;
+                                    } else {
+                                        warn!("Invalid char input: {}", buffer);
+                                        // TODO handle invalid char input (USER ERROR)
+                                    }
+                                }
+                                MavType::Array(_, _) => warn!("Array types are not supported yet"), // TODO handle array types
+                            }
                         }
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
+        }
     }
 }
