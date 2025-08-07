25e5ad7412d3795d2ec806d8abbc68a4eeea5a6b
Federico Lolli <federico.lolli@skywarder.eu>
2025-08-07 18:12:08 +0200

    Add ThreeWay colored versions

--- src/ui/panes/pid_drawing_tool/symbols/icons.rs
+++ src/ui/panes/pid_drawing_tool/symbols/icons.rs
@@ -1,16 +1,17 @@
 mod motor_valve;
 
+use std::fmt::Display;
+
 use egui::{ImageSource, Theme, Ui};
 use glam::Vec2;
 use motor_valve::MotorValve;
 use serde::{Deserialize, Serialize};
-use strum_macros::{Display, EnumIter};
 
 use crate::{mavlink::MavMessage, ui::utils::glam_to_egui};
 
 use super::SymbolBehavior;
 
-#[derive(Clone, Serialize, Deserialize, PartialEq, EnumIter, Display, Debug, Default)]
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug, Default)]
 pub enum Icon {
     #[default]
     Arrow,
@@ -24,11 +25,28 @@ pub enum Icon {
     PressureTransducer,
     QuickConnector,
     ReliefValve,
-    ThreeWayValve,
     Vessel,
 }
 
 impl Icon {
+    pub fn iter() -> Vec<Self> {
+        vec![
+            Icon::Arrow,
+            Icon::BurstDisk,
+            Icon::CheckValve,
+            Icon::FlexibleConnection,
+            Icon::ManualValve,
+            Icon::MotorValve(MotorValve::default_two_way()),
+            Icon::MotorValve(MotorValve::default_three_way()),
+            Icon::PressureGauge,
+            Icon::PressureRegulator,
+            Icon::PressureTransducer,
+            Icon::QuickConnector,
+            Icon::ReliefValve,
+            Icon::Vessel,
+        ]
+    }
+
     pub fn get_image(&self, theme: Theme) -> ImageSource {
         match (&self, theme) {
             (Icon::Arrow, Theme::Light) => {
@@ -61,42 +79,7 @@ impl Icon {
             (Icon::ReliefValve, Theme::Dark) => {
                 egui::include_image!("../../../../../icons/pid_symbols/dark/relief_valve.svg")
             }
-            (Icon::MotorValve(state), Theme::Light) => match state.last_value {
-                None => {
-                    egui::include_image!("../../../../../icons/pid_symbols/light/motor_valve.svg")
-                }
-                Some(true) => {
-                    egui::include_image!(
-                        "../../../../../icons/pid_symbols/light/motor_valve_green.svg"
-                    )
-                }
-                Some(false) => {
-                    egui::include_image!(
-                        "../../../../../icons/pid_symbols/light/motor_valve_red.svg"
-                    )
-                }
-            },
-            (Icon::MotorValve(state), Theme::Dark) => match state.last_value {
-                None => {
-                    egui::include_image!("../../../../../icons/pid_symbols/dark/motor_valve.svg")
-                }
-                Some(true) => {
-                    egui::include_image!(
-                        "../../../../../icons/pid_symbols/dark/motor_valve_green.svg"
-                    )
-                }
-                Some(false) => {
-                    egui::include_image!(
-                        "../../../../../icons/pid_symbols/dark/motor_valve_red.svg"
-                    )
-                }
-            },
-            (Icon::ThreeWayValve, Theme::Light) => {
-                egui::include_image!("../../../../../icons/pid_symbols/light/three_way_valve.svg")
-            }
-            (Icon::ThreeWayValve, Theme::Dark) => {
-                egui::include_image!("../../../../../icons/pid_symbols/dark/three_way_valve.svg")
-            }
+            (Icon::MotorValve(state), theme) => state.get_sprite(theme),
             (Icon::PressureRegulator, Theme::Light) => {
                 egui::include_image!(
                     "../../../../../icons/pid_symbols/light/pressure_regulator.svg"
@@ -147,6 +130,28 @@ impl Icon {
     }
 }
 
+impl Display for Icon {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Icon::Arrow => write!(f, "Arrow"),
+            Icon::BurstDisk => write!(f, "Burst Disk"),
+            Icon::CheckValve => write!(f, "Check Valve"),
+            Icon::FlexibleConnection => write!(f, "Flexible Connection"),
+            Icon::ManualValve => write!(f, "Manual Valve"),
+            Icon::MotorValve(internal) => match internal.variant {
+                motor_valve::MotorValveVariant::TwoWay(_) => write!(f, "Two-Way Motor Valve"),
+                motor_valve::MotorValveVariant::ThreeWay(_) => write!(f, "Three-Way Motor Valve"),
+            },
+            Icon::PressureGauge => write!(f, "Pressure Gauge"),
+            Icon::PressureRegulator => write!(f, "Pressure Regulator"),
+            Icon::PressureTransducer => write!(f, "Pressure Transducer"),
+            Icon::QuickConnector => write!(f, "Quick Connector"),
+            Icon::ReliefValve => write!(f, "Relief Valve"),
+            Icon::Vessel => write!(f, "Vessel"),
+        }
+    }
+}
+
 impl SymbolBehavior for Icon {
     fn update(&mut self, message: &MavMessage, subscribed_msg_ids: &[u32]) {
         if let Icon::MotorValve(state) = self {
@@ -191,13 +196,17 @@ impl SymbolBehavior for Icon {
                 Icon::CheckValve => vec![(0.0, 2.5), (10.0, 2.5)],
                 Icon::FlexibleConnection => vec![(0.0, 3.0), (10.0, 3.0)],
                 Icon::ManualValve => vec![(0.0, 2.5), (10.0, 2.5)],
-                Icon::MotorValve(_) => vec![(0.0, 5.0), (10.0, 5.0)],
+                Icon::MotorValve(mv) => match mv.variant {
+                    motor_valve::MotorValveVariant::TwoWay(_) => vec![(0.0, 5.0), (10.0, 5.0)],
+                    motor_valve::MotorValveVariant::ThreeWay(_) => {
+                        vec![(0.0, 3.0), (10.0, 3.0), (5.0, 8.0)]
+                    }
+                },
                 Icon::PressureGauge => vec![(3.5, 7.0)],
                 Icon::PressureRegulator => vec![(0.0, 7.0), (10.0, 7.0)],
                 Icon::PressureTransducer => vec![(3.5, 7.0)],
                 Icon::QuickConnector => vec![(0.0, 2.5), (6.0, 2.5)],
                 Icon::ReliefValve => vec![(3.0, 10.0)],
-                Icon::ThreeWayValve => vec![(0.0, 3.0), (10.0, 3.0), (5.0, 8.0)],
                 Icon::Vessel => vec![(0.0, 7.6), (8.2, 7.6), (4.1, 0.0), (4.1, 15.1)],
             }
             .iter()
@@ -213,13 +222,15 @@ impl SymbolBehavior for Icon {
             Icon::CheckValve => (10.0, 5.0),
             Icon::FlexibleConnection => (10.0, 6.0),
             Icon::ManualValve => (10.0, 5.0),
-            Icon::MotorValve(_) => (10.0, 8.0),
+            Icon::MotorValve(mv) => match mv.variant {
+                motor_valve::MotorValveVariant::TwoWay(_) => (10.0, 8.0),
+                motor_valve::MotorValveVariant::ThreeWay(_) => (10.0, 8.0),
+            },
             Icon::PressureGauge => (7.0, 7.0),
             Icon::PressureRegulator => (10.0, 10.0),
             Icon::PressureTransducer => (7.0, 7.0),
             Icon::QuickConnector => (6.0, 5.0),
             Icon::ReliefValve => (6.0, 10.0),
-            Icon::ThreeWayValve => (10.0, 8.0),
             Icon::Vessel => (8.2, 15.2),
         }
         .into()
--- src/ui/panes/pid_drawing_tool/symbols/icons/motor_valve.rs
+++ src/ui/panes/pid_drawing_tool/symbols/icons/motor_valve.rs
@@ -1,4 +1,4 @@
-use egui::{RichText, Ui, Window};
+use egui::{ImageSource, RichText, Theme, Ui, Window};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -8,18 +8,77 @@ use crate::{
     ui::cache::ChangeTracker,
 };
 
+macro_rules! load_dark_sprite {
+    ($str:expr) => {
+        egui::include_image!(concat!("../../../../../../icons/pid_symbols/dark/", $str))
+    };
+}
+
+macro_rules! load_light_sprite {
+    ($str:expr) => {
+        egui::include_image!(concat!("../../../../../../icons/pid_symbols/light/", $str))
+    };
+}
+
 #[derive(Clone, Serialize, Deserialize, PartialEq, Default, Debug)]
 pub struct MotorValve {
     subscribed_field: Option<IndexedField>,
+    pub variant: MotorValveVariant,
 
-    /// false = closed, true = open
-    #[serde(skip)]
-    pub last_value: Option<bool>,
     #[serde(skip)]
     pub is_subs_window_visible: bool,
 }
 
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
+pub enum MotorValveVariant {
+    TwoWay(TwoWayInternal),
+    ThreeWay(ThreeWayInternal),
+}
+
+impl Default for MotorValveVariant {
+    fn default() -> Self {
+        MotorValveVariant::TwoWay(TwoWayInternal::default())
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Default, Debug)]
+pub struct TwoWayInternal {
+    last_value: Option<TwoWayStates>,
+}
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Default, Debug)]
+pub struct ThreeWayInternal {
+    last_value: Option<ThreeWayStates>,
+    invert: bool,
+}
+
+#[derive(Clone, Copy, Serialize, Deserialize, PartialEq, Debug)]
+enum TwoWayStates {
+    Open,
+    Closed,
+}
+
+#[derive(Clone, Copy, Serialize, Deserialize, PartialEq, Debug)]
+enum ThreeWayStates {
+    ActiveLeft,
+    ActiveRight,
+}
+
 impl MotorValve {
+    pub fn default_two_way() -> Self {
+        Self {
+            variant: MotorValveVariant::TwoWay(TwoWayInternal::default()),
+            ..Default::default()
+        }
+    }
+
+    pub fn default_three_way() -> Self {
+        Self {
+            variant: MotorValveVariant::ThreeWay(ThreeWayInternal::default()),
+            ..Default::default()
+        }
+    }
+
     pub fn update(&mut self, msg: &MavMessage, subscribed_msg_ids: &[u32]) {
         // Reset field if msg_id has changed
         if let Some(inner_field) = &self.subscribed_field {
@@ -30,15 +89,35 @@ impl MotorValve {
 
         if let Some(field) = &self.subscribed_field {
             if field.msg_id() == msg.message_id() {
-                let value = field.extract_as_f64(msg).log_unwrap();
-                self.last_value = Some(value != 0.0);
+                let value = field.extract_as_u8(msg).log_unwrap();
+                match &mut self.variant {
+                    MotorValveVariant::TwoWay(two_way_internal) => {
+                        two_way_internal.last_value = match value {
+                            0 => Some(TwoWayStates::Closed),
+                            1 => Some(TwoWayStates::Open),
+                            _ => None,
+                        };
+                    }
+                    MotorValveVariant::ThreeWay(three_way_internal) => {
+                        three_way_internal.last_value = match (value, three_way_internal.invert) {
+                            (0, false) => Some(ThreeWayStates::ActiveLeft),
+                            (1, false) => Some(ThreeWayStates::ActiveRight),
+                            (0, true) => Some(ThreeWayStates::ActiveRight),
+                            (1, true) => Some(ThreeWayStates::ActiveLeft),
+                            _ => None,
+                        };
+                    }
+                }
             }
         }
     }
 
     pub fn reset_subscriptions(&mut self) {
         self.subscribed_field = None;
-        self.last_value = None;
+        match &mut self.variant {
+            MotorValveVariant::TwoWay(internal) => internal.last_value = None,
+            MotorValveVariant::ThreeWay(internal) => internal.last_value = None,
+        }
     }
 
     pub fn subscriptions_ui(&mut self, ui: &mut Ui, mavlink_ids: &[u32]) {
@@ -50,16 +129,67 @@ impl MotorValve {
             .movable(true)
             .open(&mut self.is_subs_window_visible)
             .show(ui.ctx(), |ui| {
-                subscription_window(ui, mavlink_ids, &mut self.subscribed_field)
+                subscription_window(
+                    ui,
+                    mavlink_ids,
+                    &mut self.variant,
+                    &mut self.subscribed_field,
+                )
             });
         // reset last_value if the subscribed field has changed
         if change_tracker.has_changed(&self.subscribed_field) {
-            self.last_value = None;
+            match &mut self.variant {
+                MotorValveVariant::TwoWay(internal) => internal.last_value = None,
+                MotorValveVariant::ThreeWay(internal) => internal.last_value = None,
+            }
+        }
+    }
+
+    pub fn get_sprite(&self, theme: Theme) -> ImageSource {
+        match (&self.variant, theme) {
+            (MotorValveVariant::TwoWay(internal), Theme::Dark) => match internal.last_value {
+                None => load_dark_sprite!("motor_valve.svg"),
+                Some(TwoWayStates::Open) => load_dark_sprite!("motor_valve_green.svg"),
+                Some(TwoWayStates::Closed) => load_dark_sprite!("motor_valve_red.svg"),
+            },
+            (MotorValveVariant::TwoWay(internal), Theme::Light) => match internal.last_value {
+                None => load_light_sprite!("motor_valve.svg"),
+                Some(TwoWayStates::Open) => load_light_sprite!("motor_valve_green.svg"),
+                Some(TwoWayStates::Closed) => load_light_sprite!("motor_valve_red.svg"),
+            },
+            (MotorValveVariant::ThreeWay(internal), Theme::Dark) => match internal.last_value {
+                None => load_dark_sprite!("three_way_valve.svg"),
+                Some(ThreeWayStates::ActiveRight) => {
+                    load_dark_sprite!("three_way_valve_active_right.svg")
+                }
+                Some(ThreeWayStates::ActiveLeft) => {
+                    load_dark_sprite!("three_way_valve_active_left.svg")
+                }
+            },
+            (MotorValveVariant::ThreeWay(internal), Theme::Light) => match internal.last_value {
+                None => load_light_sprite!("three_way_valve.svg"),
+                Some(ThreeWayStates::ActiveRight) => {
+                    load_light_sprite!("three_way_valve_active_right.svg")
+                }
+                Some(ThreeWayStates::ActiveLeft) => {
+                    load_light_sprite!("three_way_valve_active_left.svg")
+                }
+            },
         }
     }
 }
 
-fn subscription_window(ui: &mut Ui, msg_ids: &[u32], field: &mut Option<IndexedField>) {
+fn subscription_window(
+    ui: &mut Ui,
+    msg_ids: &[u32],
+    variant: &mut MotorValveVariant,
+    field: &mut Option<IndexedField>,
+) {
+    if let MotorValveVariant::ThreeWay(internal) = variant {
+        ui.checkbox(&mut internal.invert, "Invert configuration");
+        ui.add_sized([250., 10.], egui::Separator::default());
+    }
+
     let mut current_msg_id = field.as_ref().map(|f| f.msg_id()).unwrap_or(msg_ids[0]);
 
     // extract the msg name from the id to show it in the combo box
