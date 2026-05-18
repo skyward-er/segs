cc727ef225820ba87f9fcda32851daf1bd92caff
Alberto Nidasio <nidasioalberto@gmail.com>
2026-05-18 09:53:20 +0200

    add nix devshell and direnv configuration

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "SEGS devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      overlays = [ (import rust-overlay) ];
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    with pkgs;
+    {
+      devShells = forAllSystems (system: {
+        default = mkShell rec {
+          buildInputs = [
+            # Rust
+            just
+
+            # misc. libraries
+            openssl
+            pkg-config
+
+            # GUI libs
+            libxkbcommon
+            libGL
+            fontconfig
+
+            # wayland libraries
+            wayland
+
+            # x11 libraries
+            libxcursor
+            libxrandr
+            libxi
+            libx11
+
+            # Nix
+            nil # LSP
+            alejandra # Formatter
+          ];
+
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
+    };
+}
